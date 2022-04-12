f1e8de30425c953854dbe202d29e9bb14d46ac54
Frauandre_ <frauandrea2002@gmail.com>
2022-04-12 17:38:01 +0200

    m

---
+++ git history.sh
@@ -0,0 +1,115 @@
+$ history
+    1  ll
+    2  PWD
+    3  pwd
+    4  cd W:\FSD_21-23\Studente\ICTS21-23.282
+    5  pwd
+    6  cd C
+    7  mkdir versionControl
+    8  git clone https://github.com/dniero95/testVersionControl.git
+    9  ll
+   10  cd testVersionControl/
+   11  git fetch
+   12  git fetch
+   13  git fetch
+   14  git checkout branchAndrea
+   15  git branch
+   16  git checkout main
+   17  git checkout branchAndrea
+   18  nano README
+   19  ls
+   20  nano README.md
+   21  cat README.md
+   22  git pull
+   23  git add
+   24  git add README.md
+   25  git status
+   26  git commit -m "CommitAndrea"
+   27  git push
+   28  git status
+   29  git statusit push
+   30  git statusit push
+   31  git statusit push $ ls
+   32  git statusit push $ l git pushs
+   33  git push
+   34  git push
+   35  git pull
+   36  cat README.md
+   37  git status
+   38  git log
+   39  git push
+   40  ls
+   41  cd documenti
+   42  cd Documenti
+   43  cd Documenti/
+   44  cd /Documenti
+   45  ls
+   46  ls
+   47  cd ..
+   48  ls
+   49  cd C:\Users\ICTS21-23.282\Documents\GitHub
+   50  cd C:\Users\ICTS21-23.282\Documents\
+   51  cd \Users\ICTS21-23.282\Documents\
+   52  cd \Users\ICTS21-23.282\Documents
+   53  cd \Users\ICTS21-23.282\Documenti
+   54  cd C:
+   55  ls
+   56  cd Users
+   57  ls
+   58  cd ICTS21-23.282/
+   59  ls
+   60  cd Document
+   61  cd Documenti
+   62  ls
+   63  cd GitHub/
+   64  git clone https://github.com/Kindaglia/KiFrCa.git
+   65  cd KiFrCa/
+   66  git fetch
+   67  ls
+   68  cd TestFunzioni/
+   69  ls
+   70  cd TestFunzioni
+   71  ls
+   72  code Program.cs
+   73  git fetch
+   74  git fetch
+   75  git pull
+   76  git checkout
+   77  git checkout Frau
+   78  ls
+   79  git commit
+   80  git commit -m ModificaFrau
+   81  ls
+   82  git add Program.cs
+   83  git commit -m ModificaFrau
+   84  git push
+   85  code Program.cs
+   86  git merge main
+   87  history
+   88  ls
+   89  cd GitHub/
+   90  cd C:
+   91  cd Users
+   92  cd Documenti
+   93  cd ICTS21-23.282/
+   94  cd Documenti
+   95  cd GitHub/
+   96  git clone https://github.com/ItsAndreaCaglioti/HeyDragonessa.git
+   97  git fetch
+   98  cd HeyDragonessa/
+   99  git fetch
+  100  git checkout
+  101  git checkout Frau
+  102  ls
+  103  code index.html
+  104  git add index.html
+  105  git commit -m Uaglioo
+  106  git push
+  107  code index.html
+  108  git fetch
+  109  git pull
+  110  ls
+  111  cat index.html
+  112  git fetch
+  113  git merge main Frau
+  114  cat index.html
