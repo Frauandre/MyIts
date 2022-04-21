5065049df05c6cfe2afde87818af6ab4c4c9ae06
Frauandre_ <frauandrea2002@gmail.com>
2022-04-21 09:09:10 +0200

    pp

---
+++ Programmazione/C#/Magazzino/Magazzino/ProdottiBiz.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magazzino
+{
+    class ProdottiBiz
+    {
+        //classe business Logic Layer
+        //Serve per implementare servizi su collezioni di dati
+
+        public List<Prodotto> Elenco { get; set; }
+        public ProdottiBiz()
+        {
+            Elenco = new List<Prodotto>();
+        }
+
+        public Prodotto PrezzoMax()
+        {
+            int pos = 0; //posizione con prezzo massimo
+            for (int i = 1; i < Elenco.Count; i++)
+                if (Elenco[i].Prezzo > Elenco[pos].Prezzo)
+                    pos = i;
+            return Elenco[pos];
+
+            /*
+            Prodotto prodotto = Elenco[0];
+            foreach (var p in Elenco)
+                if (p.Prezzo > prodotto.Prezzo)   Con il foreach
+                    prodotto = p;
+            return prodotto;
+            */
+        }
+    }
+}
---
+++ Programmazione/C#/Magazzino/Magazzino/Prodotto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magazzino
+{
+    class Prodotto
+    {
+        public int Codice { get; set; }
+        public string Nome { get; set; }
+        public string Descrizione { get; set; }
+        public double Prezzo { get; set; }
+        public int Giacenza { get; set; }
+
+        public bool InScorta()
+        {
+            return giacenza >=
+        }
+        public override string ToString()
+        {
+            return $"{{{nameof(codice)}={codice.ToString()}," +
+                $" {nameof(nome)}={nome}, " +
+                $"{nameof(descrizione)}={descrizione}," +
+                $" {nameof(prezzo)}={prezzo.ToString()}," +
+                $" {nameof(giacenza)}={giacenza.ToString()}}}";
+        }
+    }
+}
