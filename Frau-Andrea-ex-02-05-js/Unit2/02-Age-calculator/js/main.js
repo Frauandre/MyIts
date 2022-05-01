50cf780ebe0b91069658c5c016922415e6bc3962
Frauandre_ <frauandrea2002@gmail.com>
2022-05-01 21:44:49 +0200

    p

---
+++ Frau-Andrea-ex-02-05-js/Unit2/02-Age-calculator/js/main.js
@@ -0,0 +1,14 @@
+let MyYear;
+MyYear= 2002;
+
+let FutureYear;
+FutureYear= 2051;
+
+let FutureAge;
+FutureAge= FutureYear-MyYear;
+
+console.log('Your birth year: ', MyYear);
+console.log('Year selected: ', FutureYear);
+console.log('Your future age ', FutureAge);
+console.log('I will be either 49 in 2051');
+
